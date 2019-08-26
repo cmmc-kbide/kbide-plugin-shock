c3bc060c307e30fc9e749de5e62c0643021caeb8
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 13:38:56 +0700

    add renerator

--- blocks/blocks_shock.js
+++ blocks/blocks_shock.js
@@ -1,15 +1,20 @@
-Blockly.Blocks['ky_002_vibration_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-002 VIBRATION");
-    this.appendValueInput("KY_002_VIBRATION_PIN")
-        .setCheck("Number")
-        .appendField("PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_002_vibration_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-002 VIBRATION");
+      this.appendDummyInput()
+        .appendField("PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "KY_002_VIBRATION_PIN");
+      this.appendDummyInput()
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_002_VIBRATION_VALUE"),
+          "KY_002_VIBRATION_VALUE"
+        );
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_shock.js
+++ blocks/generators_shock.js
@@ -1,6 +1,20 @@
-Blockly.JavaScript['ky_002_vibration_block'] = function(block) {
-  var value_ky_002_vibration_pin = Blockly.JavaScript.valueToCode(block, 'KY_002_VIBRATION_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_002_vibration_block"] = function(block) {
+    var text_ky_002_vibration_pin = block.getFieldValue("KY_002_VIBRATION_PIN");
+    var variable_ky_002_vibration_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_002_VIBRATION_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+    #VARIABLE
+		int KY_002_VIBRATION_PIN = ${text_ky_002_vibration_pin}; // define the sensor Pin 
+	#END
+
+	#SETUP
+		pinMode (KY_002_VIBRATION_PIN, INPUT); // input from KY-002 sensor
+	#END
+
+	${variable_ky_002_vibration_value} = digitalRead (KY_002_VIBRATION_PIN); // read the value from KY-002
+    `;
+    return code;
+  };
